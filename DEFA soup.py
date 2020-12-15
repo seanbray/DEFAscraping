bd2e66ecdc7143eac71fb386e352dd3278ee5d32
seanbray <sean.bray13@gmail.com>
2020-12-15 11:31:58 -0700

    initial branch
    Working on a better way (eg df-less) to grab  the film roles
    - Able to grab role titles by iterating through captions
    - next step to grab spans under captions

--- DEFA soup.py
+++ DEFA soup.py
@@ -14,11 +14,6 @@ with open("output.txt", "r") as f:
     for item in f:
         url_list.append(str.rstrip(item))
 
-# create a dataframe here, then fill it in the following for loop, then print to excel there's almost certainly a
-# better way, but this works well enough for my purposes, and I've only just started learning pandas
-defa_df = pd.DataFrame(columns= ['Title', 'Year', 'Runtime', 'Format', 'Director', 'Script', 'Dramaturg', 'Editor', 'Camera', 'Set Design',
-           'Costume Design', 'Music (Score)', 'Cast', 'Producer'], index=['Film'])
-print(defa_df)
 n = 0
 
 # this for loop works through all of the URLs on my list, grabs the film title, then populates the production roles
@@ -31,20 +26,16 @@ for url in url_list:
     year = soup.select('span')[2].text
     runtime = soup.select('span')[4].text
     format = soup.select('span')[6].text
+    captions = soup.findAll('caption')
+    for n in range(len(captions)):
+        role = soup.select('caption')[n].text
+        caption1 = soup.find_all('caption')[n]
+        # caption2 = soup.find_all('caption')[n+1] # need to find a way to grab all spans between these captions
+        foundRole = caption1.find_next_sibling.string
+        print(foundRole)
     info = {'Title': [title], 'Year': [year], 'Runtime': [runtime], 'Format': [format]}
-    film_df = pd.DataFrame(info, columns= ['Title', 'Year', 'Runtime', 'Format'])
-    # because the roles for a given film can vary, the above doesn't work well to get them. hence the following:
-    for role in credits: # this method is somewhat flawed in that it can only grab the first person for each role,
-        # no good solution yet
-        try:
-            dfs = pd.read_html(html, match=role, skiprows=0)
-            df = dfs[0]
-            df.columns = [role]
-            film_df.insert(1, role, df, allow_duplicates=True)
-        except:
-            film_df.insert(1, role, 'NaN')
-        defa_df = pd.concat([defa_df, film_df], sort=True) # and here we expand the master defa dataframe
+    print(info)
     n = n+1
     print(str((n/1099)*100) + "%") # reports the % completion, helps track progress
 
-defa_df.to_excel(r'D:\Dropbox\2020 Fall\DHUM\Portfolio\Git\DEFAscraping\export.xlsx', startrow=0, index = False,header= True)
+# defa_df.to_excel(r'D:\Dropbox\2020 Fall\DHUM\Portfolio\Git\DEFAscraping\export.xlsx', startrow=0, index = False,header= True)
